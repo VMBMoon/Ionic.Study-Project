b03b082fbf76885a0d30cfa263af0447dca43093
HisuiKitsune <foxnightly2010@gmail.com>
2023-01-17 17:00:06 -0300

    feat: register page optmization

--- src/app/pages/register/register.page.spec.ts
+++ src/app/pages/register/register.page.spec.ts
@@ -1,3 +1,5 @@
+import { AppRoutingModule } from './../../app-routing.module';
+import { Router } from '@angular/router';
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
@@ -6,19 +8,26 @@ import { RegisterPage } from './register.page';
 describe('RegisterPage', () => {
   let component: RegisterPage;
   let fixture: ComponentFixture<RegisterPage>;
+  let router: Router
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ RegisterPage ],
-      imports: [IonicModule.forRoot()]
+      imports: [IonicModule.forRoot(), AppRoutingModule]
     }).compileComponents();
 
     fixture = TestBed.createComponent(RegisterPage);
+    router = TestBed.inject(Router);
+
     component = fixture.componentInstance;
     fixture.detectChanges();
   }));
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should go to login page on register', () => {
+    spyOn(router, 'navigate');
+
+    component.register();
+
+    expect(router.navigate).toHaveBeenCalledWith(['login']);
   });
 });

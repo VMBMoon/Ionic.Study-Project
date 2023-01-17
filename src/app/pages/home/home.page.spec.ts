2675a3b0b9cc9767313133914ff730f1b939ed2b
HisuiKitsune <foxnightly2010@gmail.com>
2023-01-17 16:59:14 -0300

    feat: new functions

--- src/app/pages/home/home.page.spec.ts
+++ src/app/pages/home/home.page.spec.ts
@@ -1,4 +1,6 @@
+import { AppRoutingModule } from './../../app-routing.module';
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { Router } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 
 import { HomePage } from './home.page';
@@ -6,19 +8,34 @@ import { HomePage } from './home.page';
 describe('HomePage', () => {
   let component: HomePage;
   let fixture: ComponentFixture<HomePage>;
+  let router: Router;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ HomePage ],
-      imports: [IonicModule.forRoot()]
+      imports: [IonicModule.forRoot(), AppRoutingModule]
     }).compileComponents();
 
     fixture = TestBed.createComponent(HomePage);
+    router = TestBed.inject(Router);
+
     component = fixture.componentInstance;
     fixture.detectChanges();
   }));
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should go to sales page on see details', () => {
+    spyOn(router, 'navigate');
+
+    component.goToSalesDetail();
+
+    expect(router.navigate).toHaveBeenCalledWith(['sales']);
+  });
+
+  it('should go to add sale on create sale', () => {
+    spyOn(router, 'navigate');
+
+    component.newSale();
+
+    expect(router.navigate).toHaveBeenCalledWith(['add-sale']);
   });
 });
